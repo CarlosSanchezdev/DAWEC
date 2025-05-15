4da9a91cae0a742d4df4504b7774f1dc6dac96a0
CarlosSanchezdev <carlos.yerga.dev@gmail.com>
2025-05-15 15:19:08 +0200

    type(scope): subject
    Max length: 30 characters
    
    - feat: New features
    - fix: Bug fixes
    - docs: Documentation changes
    - style: Code style changes
    - refactor: Code refactoring
    - test: Testing changes
    - chore: Maintenance tasks
    
    - tema1 through tema8 (tema1, tema2, etc.)
    - sim1 through sim8 (sim1, sim2, etc.)
    
    - feat(tema1): add useState component
    - fix(tema2): correct array logic
    - docs(sim1): update readme
    - style(tema3): format code

---
+++ .github/commit-template.md
@@ -0,0 +1,25 @@
+# Commit Template
+
+## Format
+type(scope): subject
+
+Max length: 30 characters
+
+## Valid Types
+- feat: New features
+- fix: Bug fixes
+- docs: Documentation changes
+- style: Code style changes
+- refactor: Code refactoring
+- test: Testing changes
+- chore: Maintenance tasks
+
+## Valid Scopes
+- tema1 through tema8 (tema1, tema2, etc.)
+- sim1 through sim8 (sim1, sim2, etc.)
+
+## Examples
+- feat(tema1): add useState component
+- fix(tema2): correct array logic
+- docs(sim1): update readme
+- style(tema3): format code
--- commitlint.config.js
+++ commitlint.config.js
@@ -1,44 +1,17 @@
-module.exports = {
+export default {
     extends: ['@commitlint/config-conventional'],
     rules: {
-        'type-enum': [
-            2,
-            'always',
-            [
-                'feat',
-                'fix',
-                'docs',
-                'style',
-                'refactor',
-                'perf',
-                'test',
-                'chore',
-                'ci',
-                'revert'
-            ]
-        ],
-        'scope-enum': [
-            2,
-            'always',
-            [
-                'auth',
-                'ui',
-                'api',
-                'router',
-                'store',
-                'utils',
-                'config',
-                'deps'
-            ]
-        ],
-        'subject-case': [
-            2,
-            'always',
-            ['lower-case']
-        ],
-        'subject-empty': [2, 'never'],
+        'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore']],
+        'scope-enum': [2, 'always', [
+            'tema1', 'tema2', 'tema3', 'tema4', 'tema5', 'tema6', 'tema7', 'tema8',
+            'sim1', 'sim2', 'sim3', 'sim4', 'sim5', 'sim6', 'sim7', 'sim8'
+        ]],
+        'type-case': [2, 'always', 'lower'],
+        'scope-case': [2, 'always', 'lower'],
+        'scope-empty': [2, 'never'],
         'type-empty': [2, 'never'],
+        'subject-empty': [2, 'never'],
         'subject-full-stop': [2, 'never', '.'],
-        'header-max-length': [2, 'always', 72]
+        'header-max-length': [2, 'always', 30]
     }
 };
